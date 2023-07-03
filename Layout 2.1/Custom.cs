d4df972c13c63ab38028b6a874239f8aa0197a2e
Bhima86 <patilBhimashankar09@gamil.com>
2023-07-03 11:45:41 +0530

    Added Custom Errors and tested on project

---
+++ Layout 2.1/Custom.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Layout_2._1
+{
+    public class Custom
+    {
+        public static void ErrorHandle(Exception ex, HttpResponse Response)
+        {
+            // Determine the appropriate status code based on the exception
+            int statusCode;
+            if (ex is HttpException)
+            {
+                statusCode = 404; // File Not Found
+            }
+            else if (ex is UnauthorizedAccessException)
+            {
+                statusCode = 401; // Unauthorized
+            }
+            else
+            {
+                statusCode = 500; // Internal Server Error
+            }
+
+            //Set the response status code
+            Response.StatusCode = statusCode;
+
+            // Redirect to the corresponding error page based on the status code
+            switch (statusCode)
+            {
+                case 404:
+                    Response.Redirect("~/Error Handler/Error404.aspx");
+                    break;
+                case 500:
+                    Response.Redirect("~/Error Handler/Error500.aspx");
+                    break;
+                default:
+                    Response.Redirect("~/Error Handler/Error.aspx");
+                    break;
+            }
+        }
+    }
+}
---
+++ Layout 2.1/Error Handler/Error500.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Error500.aspx.cs" Inherits="Layout_2._1.Error_Handler.Error500" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+                <h2 style="color:red"><strong>500 Internal Server Error server </strong></h2>
+                <p>The requested action is not possible.</p>
+                <p>Try after sometime or try go to the <a href="../Calendar 1.aspx">Calendar page</a>.</p>
+        </div>
+    </form>
+</body>
+</html>
