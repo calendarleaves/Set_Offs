5ae92e4a1fbb533b8591403286919c792ae01e49
Suraj Patil <surajpatil8252@gmail.com>
2023-07-04 18:49:23 +0530

    Updated add leave page and added cancel event

--- Layout 2.1/AddLeave.aspx.designer.cs
+++ Layout 2.1/AddLeave.aspx.designer.cs
@@ -24,6 +24,24 @@ namespace Layout_2._1
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// DropDownList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+        /// <summary>
         /// Label2 control.
         /// </summary>
         /// <remarks>
