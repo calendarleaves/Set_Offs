f2b2808051861f1359b6c95db26cc3a6cd98f3b6
swapnil <swapnilsuradkar610@gmail.com>
2023-07-25 17:46:35 +0530

    Holiday pop up added

--- Layout 2.1/Calendar 1.aspx.designer.cs
+++ Layout 2.1/Calendar 1.aspx.designer.cs
@@ -130,5 +130,14 @@ namespace WebApplication1
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Holidays control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Layout_2._1.Holidays Holidays;
     }
 }
