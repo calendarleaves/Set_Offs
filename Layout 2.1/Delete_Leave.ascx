cc457b8cb83340651c71dddb4942fec4d9a2e899
Pooja Devare <poojadevare023@gmail.com>
2023-07-27 15:44:08 +0530

    Delete Leave PopUp Code Pages

--- Layout 2.1/Delete_Leave.ascx
+++ Layout 2.1/Delete_Leave.ascx
@@ -2,54 +2,68 @@
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-KyZXEAgltcdPwqR5gKkxzWfod5oMh4PrpQmYfjELVYg=" crossorigin="anonymous"></script>
 
-<!-- Place this script block after adding the jQuery library -->
-<script>
-    $(document).ready(function () {
-        // When a date is clicked in the calendar, prevent the default postback behavior
-        $('#<%= Calendar1.ClientID %> td:not(.ajax__calendar_other)').on('click', function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-        });
-    });
-</script>
+
+
+
 
 <div class="modal" id="myModal4">
     <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content"  style="height:650px; width:1000px;">
             <!-- Modal Header -->
-            <div class="modal-header">
-                <h4 class="modal-title">Delete Leave Form</h4>
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-            </div>
+            
 
             <!-- Modal body -->
             <div class="modal-body">
-                <!-- Your form content here -->
-                <asp:TextBox ID="txtName" runat="server" CssClass="form-control" placeholder="Enter your name"></asp:TextBox>
-                <!-- Add other form elements here -->
-                <asp:Button ID="Button1" runat="server" Text="Add" OnClick="Button1_Click" />
+                 
+                <div class="container ">
+            <h2 class="text-center">Delete Leave</h2>
+            <hr />
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2">
+                    <asp:Label ID="lblErrorMessage" runat="server" ForeColor="Red" Font-Size="15px" display="flex"></asp:Label>
+                    <div class="input-group">
+                        <asp:TextBox runat="server" ID="txtSearch" class="form-control" placeholder="Search employee name"></asp:TextBox>
+                        <div class="input-group-append">
+                            <asp:Button runat="server" ID="btnSearch" class="btn btn-primary" Text="Search" OnClick="btnSearch_Click" />
+
+                            <div class="col-lg-12 text-right">
+
+                                <asp:Button runat="server" ID="btnRefresh" CssClass="refreshButton" Text="Refresh" OnClick="btnRefresh_Click" />
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
-            <asp:Calendar ID="Calendar1" CssClass="calendarView" DayNameFormat="FirstLetter"     runat="server" BackColor="white"   CellPadding="4" 
-      BorderColor="#999999" Font-Names="Verdana" Font-Size="8pt" 
-      Height="180px" ForeColor="Black" 
-      Width="200px" >
-      <TodayDayStyle ForeColor="Black" BackColor="#CCCCCC"></TodayDayStyle>
-      <SelectorStyle BackColor="#CCCCCC"></SelectorStyle>
-      <NextPrevStyle VerticalAlign="Bottom"></NextPrevStyle>
-      <DayHeaderStyle Font-Size="8pt"  Font-Bold="false" BackColor="#CCCCCC">
-      </DayHeaderStyle>
-      <SelectedDayStyle Font-Bold="True" ForeColor="White" BackColor="#666666">
-      </SelectedDayStyle>
-      <TitleStyle Font-Bold="True" BorderColor="Black" BackColor="white">
-      </TitleStyle>
-      <WeekendDayStyle BackColor="white"></WeekendDayStyle>
-      <OtherMonthDayStyle ForeColor="#808080"></OtherMonthDayStyle> </asp:Calendar> 
+            <!--Gridview Container  -->
+            <div class="col-lg-10 offset-lg-1 mt-4 text-center">
+                <div class="gridview-container">
+                    <asp:GridView ID="DeleteLeaveGridView" runat="server" AutoGenerateColumns="false" CssClass="table" OnRowDeleting="DeleteLeaveGridView_RowDeleting" DataKeyNames="Id" OnRowDataBound="DeleteLeaveGridView_RowDataBound" OnSelectedIndexChanged="DeleteLeaveGridView_SelectedIndexChanged">
+                        <Columns>
+                            <asp:BoundField DataField="Id" HeaderText="Emp ID" />
+                            <asp:BoundField DataField="FirstName" HeaderText="First Name" />
+                            <asp:BoundField DataField="LastName" HeaderText="Last Name" />
+                            <asp:BoundField DataField="LeaveType" HeaderText="Leave Type" />
+                            <asp:BoundField DataField="StartDate" HeaderText="Start Date" DataFormatString="{0:dd/MM/yyyy}" />
+                            <asp:BoundField DataField="EndDate" HeaderText="End Date" DataFormatString="{0:dd/MM/yyyy}" />
+                            <asp:BoundField DataField="Days" HeaderText="Total Days" />
+                            <asp:TemplateField HeaderText="Action">
+                                <ItemTemplate>
+                                    <asp:Button CommandName="Delete" runat="server" CssClass="btn btn-danger" Text="Delete" />
+                                         <%--OnClientClick='<%# "return showConfirmationAndAlert(\"" +  Eval("FirstName") + " " + Eval("LastName") + "\", \"" + Eval("StartDate") + "\", \"" + Eval("EndDate") + "\");" %>'--%>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                    </asp:GridView>
+                    <asp:Label ID="lblmessage" runat="server" Font-Bold="true" display="flex"></asp:Label>
+                </div>
+            </div>
+
+        </div>               </div>        
 
             <!-- Modal footer -->
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <!-- You can add additional buttons if needed -->
-            </div>
+            
         </div>
     </div>
 </div>
--- Layout 2.1/Delete_Leave.ascx.cs
+++ Layout 2.1/Delete_Leave.ascx.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using SetOffs1;
 
 namespace Layout_2._1
 {
@@ -12,81 +15,180 @@ namespace Layout_2._1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                BindGridView();
+            }
         }
 
-        protected void Button1_Click(object sender, EventArgs e)
+        private void BindGridView()
         {
-            string connectionString = "Data Source=DESKTOP-45HGR8T;Initial Catalog=Setoffs;Integrated Security=True";
-
-            // Get the value from the text box
-            string nameValue = txtName.Text;
-
-            // Create the SQL query to insert the value into the table
-            string insertQuery = "INSERT INTO Trial1 (name) VALUES (@Name)";
-
             try
             {
-                // Create a new SqlConnection using the connection string
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                DataTable dt = new DataTable();
+                DBConnection con = new DBConnection();
+
+                dt = con.GetAllEmployeesLeave();
+                if (dt.Rows.Count == 0)
                 {
-                    // Open the connection
-                    connection.Open();
+                    DeleteLeaveGridView.DataSource = null;
+                    DeleteLeaveGridView.DataBind();
+                    lblmessage.Text = "No Records!";
+                    ScriptManager.RegisterStartupScript(this, GetType(), "keepModalOpen", "$('#myModal6').modal('show');", true);
 
-                    // Create a new SqlCommand with the query and the connection
-                    using (SqlCommand command = new SqlCommand(insertQuery, connection))
-                    {
-                        // Add a parameter to the command to avoid SQL injection
-                        command.Parameters.AddWithValue("@Name", nameValue);
-
-                        // Execute the query
-                        command.ExecuteNonQuery();
-                    }
                 }
-
-                // If the execution reaches here, the insertion was successful
-                // You can add any additional code or messages if needed
-                Response.Write("Data inserted successfully!");
+                else
+                {
+                    DeleteLeaveGridView.DataSource = dt;
+                    DeleteLeaveGridView.DataBind();
+                    lblmessage.Text = string.Empty;
+                    lblmessage.Visible = false;
+                    ScriptManager.RegisterStartupScript(this, GetType(), "keepModalOpen", "$('#myModal6').modal('show');", true);
+                }
             }
             catch (Exception ex)
             {
-                // Handle any exceptions that might occur during the database operation
-                // You can display an error message or log the exception details
-                Response.Write("An error occurred: " + ex.Message);
+                Custom.ErrorHandle(ex, Response);
             }
-
-            DateTime selectedDate = Calendar1.SelectedDate;
-
-            // Insert the selected date into the Trial1 table
-            InsertIntoTrial1(selectedDate);
-
-            // Optionally, close the modal pop-up after inserting the date
-            //PopupForm.Hide();
         }
 
-        private void InsertIntoTrial1(DateTime selectedDate)
+        protected void btnSearch_Click(object sender, EventArgs e)
         {
-            // Implement your database connection and insert logic here.
-            // This is just a sample to demonstrate the concept.
-
-            // For simplicity, assume you are using SqlConnection and SqlCommand
-            string connectionString = "Data Source=DESKTOP-45HGR8T;Initial Catalog=Setoffs;Integrated Security=True";
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            try
             {
-                string insertQuery = "INSERT INTO Trial1 (Date) VALUES (@SelectedDate)";
-                SqlCommand command = new SqlCommand(insertQuery, connection);
-                command.Parameters.AddWithValue("@SelectedDate", selectedDate);
 
-                try
+                DataTable dt = new DataTable();
+                DBConnection con = new DBConnection();
+
+                string searchText = txtSearch.Text.Trim();
+                Regex regex = new Regex("^[A-Za-z\\s]+$");
+
+                if (string.IsNullOrEmpty(txtSearch.Text))
                 {
-                    connection.Open();
-                    command.ExecuteNonQuery();
+                    lblErrorMessage.Text = "Enter Employee Name..";
+                    txtSearch.Focus();
+
                 }
-                catch (Exception ex)
+                else if (!regex.IsMatch(searchText))
                 {
-                    // Handle the exception appropriately.
+                    lblErrorMessage.Visible = true;
+                    lblErrorMessage.Text = "Wrong input! Only letters  are allowed.";
+                    lblmessage.Visible = false;
+                    return;
+
                 }
+                else
+                {
+                    dt = con.GetAllEmployeesLeaveLikeName(searchText);
+
+                    if (dt.Rows.Count == 0)
+                    {
+                        DeleteLeaveGridView.DataSource = null;
+                        DeleteLeaveGridView.DataBind();
+                        lblmessage.Visible = true;
+                        lblmessage.Text = "No Records!";
+                        lblErrorMessage.Text = string.Empty;
+                        lblErrorMessage.Visible = false;
+
+                    }
+                    else
+                    {
+                        DeleteLeaveGridView.DataSource = dt;
+                        DeleteLeaveGridView.DataBind();
+                        lblmessage.Text = string.Empty;
+                        lblmessage.Visible = false;
+                        lblErrorMessage.Text = string.Empty;
+                        lblmessage.Visible = false;
+
+                    }
+                }
+                ScriptManager.RegisterStartupScript(this, GetType(), "keepModalOpen", "$('#myModal6').modal('show');", true);
+            }
+            catch (Exception ex)
+            {
+                Custom.ErrorHandle(ex, Response);
             }
         }
+
+        protected void DeleteLeaveGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+
+            try
+            {
+
+                //int id = Convert.ToInt32(DeleteLeaveGridView.Rows[e.RowIndex].Cells[0].Text);
+                string fullName = DeleteLeaveGridView.Rows[e.RowIndex].Cells[1].Text + " " + DeleteLeaveGridView.Rows[e.RowIndex].Cells[2].Text;
+                string startDate = DeleteLeaveGridView.Rows[e.RowIndex].Cells[4].Text;
+                string endDate = DeleteLeaveGridView.Rows[e.RowIndex].Cells[5].Text;
+
+                DBConnection con = new DBConnection();
+                con.DeleteLeave(fullName, DateTime.Parse(startDate), DateTime.Parse(endDate));
+
+                //string successMessage = "Record has been deleted.";
+                //string script = $"<script>showSuccessAlert(\"{successMessage}\");</script>";
+                //ClientScript.RegisterStartupScript(GetType(), "ShowSuccessAlert", script);
+                //ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Record Deleted Successfully')", true);
+                BindGridView();
+
+            }
+            catch (Exception ex)
+            {
+                //string errorMessage = "Failed to delete the record.";
+                //string script = $"<script>showWarningAlert(\"{errorMessage}\");</script>";
+                //ClientScript.RegisterStartupScript(GetType(), "ShowWarningAlert", script);
+
+                Custom.ErrorHandle(ex, Response);
+            }
+            ScriptManager.RegisterStartupScript(this, GetType(), "keepModalOpen", "$('#myModal6').modal('show');", true);
+
+        }
+
+        protected void btnRefresh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                lblmessage.Text = string.Empty;
+                lblmessage.Visible = false;
+                lblErrorMessage.Text = string.Empty;
+                lblmessage.Visible = false;
+                txtSearch.Text = "";
+                txtSearch.Focus();
+                BindGridView();
+
+            }
+            catch (Exception ex)
+            {
+                Custom.ErrorHandle(ex, Response);
+            }
+            ScriptManager.RegisterStartupScript(this, GetType(), "keepModalOpen", "$('#myModal6').modal('show');", true);
+        }
+
+        protected void DeleteLeaveGridView_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+
+                if (e.Row.RowIndex == DeleteLeaveGridView.SelectedIndex)
+                {
+                    e.Row.CssClass = "selected-row";
+                }
+            }
+            ScriptManager.RegisterStartupScript(this, GetType(), "keepModalOpen", "$('#myModal6').modal('show');", true);
+        }
+
+        protected void DeleteLeaveGridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //String script = "$('#myModal6').modal('show')";
+            //ClientScriptManager.RegisterStartupScript(this.GetType(), "keepModalOpen", script,true);
+            // ScriptManager.RegisterStartupScript(this, GetType(), "keepModalOpen", "$('.modal').modal('show');", true);
+            DeleteLeaveGridView.SelectedIndex = DeleteLeaveGridView.SelectedRow.RowIndex;
+            ScriptManager.RegisterStartupScript(this, GetType(), "keepModalOpen", "$('#myModal6').modal('show');", true);
+        }
+
+
+
+
     }
 }
