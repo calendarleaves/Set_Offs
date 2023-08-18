4b54c671ab5dc53826a67c1f2068d3a8b898666a
Bhima86 <patilbhimashankar09@gmail.com>
2023-08-18 11:17:23 +0530

    commited header at center

--- Layout 2.1/Calendar 1.aspx.designer.cs
+++ Layout 2.1/Calendar 1.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace WebApplication1
         protected global::System.Web.UI.WebControls.Image Image2;
 
         /// <summary>
+        /// Image5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image5;
+
+        /// <summary>
         /// holi control.
         /// </summary>
         /// <remarks>
